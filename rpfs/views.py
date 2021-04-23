3ceb6ac6a88b356622e95ab9af6e66458e88fdbb
Katty <kross1727@gmail.com>
2021-04-23 02:41:14 -0500

    VERSION

--- rpfs/views.py
+++ rpfs/views.py
@@ -1,13 +1,22 @@
-from django.template.response import TemplateResponse
-from django.views.generic.base import View
+from django.shortcuts import get_object_or_404, render
+
+from .models import Category, Product
 
 
-class HomeView(View):
-    
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return TemplateResponse(
-                   request,
-                   'home.html',
-                   context=context,
-                   status=200)        
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'rpfs/home.html', {'products': products})
+
+def category_list(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'rpfs/products/category.html', {'category': category, 'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, still_avail=True)
+    return render(request, 'rpfs/products/detail.html', {'product': product})     
