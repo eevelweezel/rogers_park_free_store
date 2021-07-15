4cb23c04e1469f042a02c4c6ec8e27850434325c
Katty <kross1727@gmail.com>
2021-07-14 21:23:30 -0500

    Translate Templates & Forms

--- rpfs/forms.py
+++ rpfs/forms.py
@@ -2,7 +2,9 @@ from captcha.fields import CaptchaField
 from django import forms
 from django.conf import settings
 from django.core.mail import send_mail
+from django.utils.translation import gettext_lazy as _
 from .models import Category
+
 
 class CaptchaContactForm(forms.Form):
 
@@ -27,10 +29,10 @@ class ContactForm(CaptchaContactForm):
         super().__init__(*args, **kwargs)
         self.subject = 'Contact inquiry'
 
-    name = forms.CharField(label='Your name', required=True)
-    email = forms.EmailField(label='Email', required=True)
-    subject = forms.CharField(label='Subject', required=True)
-    message = forms.CharField(label='Message', widget=forms.Textarea, required=True) 
+    name = forms.CharField(label=_('Your name'), required=True)
+    email = forms.EmailField(label=_('Email'), required=True)
+    subject = forms.CharField(label=_('Subject'), required=True)
+    message = forms.CharField(label=_('Message'), widget=forms.Textarea, required=True) 
     #category = forms.ChoiceField(choices[('general information', 'General Information'), ('donation', 'Donation'), ('pickup', 'Pickup'), ('other', 'Other')])
     #if I wasn't using a Django widgit, would I need to specift a char_limit for "message"? 
 
@@ -38,25 +40,22 @@ class VolunteerForm(CaptchaContactForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.subject = 'Volunteer inquiry'
+        self.subject =_('Volunteer inquiry')
 
-    name = forms.CharField(label='Your name', required=True) 
-    email = forms.EmailField(label='Email', required=True)
-    pronouns = forms.CharField(label='Pronouns', required=True)
-    message = forms.CharField(label='Message', widget=forms.Textarea, required=True)
+    name = forms.CharField(label=_('Your name'), required=True) 
+    email = forms.EmailField(label=_('Email'), required=True)
+    pronouns = forms.CharField(label=_('Pronouns'), required=True)
+    message = forms.CharField(label=_('Message'), widget=forms.Textarea, required=True)
     VOLUNTEER_CHOICES = (
-            ('a', 'Working at the Free Store location during "closed to public" times (organizing the space, putting supply packages together, etc.)'),
-            ('b', 'Working at the Free Store location during "open to public" times (handing out requested supplies, helping neighbors make supply requests, etc.)'),
-            ('c', 'Delivering supplies either from or to the Free Space (will need a car)'),
-            ('d', 'Volunteer Coordination/Training/Support (can be working from home)'),
-            ('e', 'Inventory and/or financial book-keeping (can be working from home)'),
-            ('f', 'Outreach and/or social media (can be working from home)'),
-            ('g', 'Translation (Spanish, French, Chinese, others if applicable)'),
-            ('h', 'Other'),
+            ('a', _('Working at the Free Store location during "closed to public" times (organizing the space, putting supply packages together, etc.)')),
+            ('b', _('Working at the Free Store location during "open to public" times (handing out requested supplies, helping neighbors make supply requests, etc.)')),
+            ('c', _('Delivering supplies either from or to the Free Space (will need a car)')),
+            ('d', _('Volunteer Coordination/Training/Support (can be working from home)')),
+            ('e', _('Inventory and/or financial book-keeping (can be working from home)')),
+            ('f', _('Outreach and/or social media (can be working from home)')),
+            ('g', _('Translation (Spanish, French, Chinese, others if applicable)')),
+            ('h', _('Other')),
     )
-    volunteer_field = forms.MultipleChoiceField(label='Volunteer Positions', choices = VOLUNTEER_CHOICES)
+    volunteer_field = forms.MultipleChoiceField(label=_('Volunteer Positions'), widget=forms.CheckboxSelectMultiple, choices = VOLUNTEER_CHOICES)
 
     
-    #how render this form into a view??? Do we need to create code in views.py?
-
-#    title = forms.MultipleChoiceField(choices =VOLUNTEER_CHOICES)
